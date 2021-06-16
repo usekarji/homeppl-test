7543292d319ccf0a9afd03511036df8419926de5
msekarji <msekarji@tocafootball.com>
2021-06-16 11:46:36 +0100

    Refactored providers and backend into main.tf

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Two public subnets in each of the VPCs and internet gateway enabled
 ALB on the main VPC which redirects the incoming requests to the underlying apache instance
 
 
+#Deployment steps
+1) Create your aws profile 
+
--- main.tf
+++ main.tf
@@ -11,3 +11,13 @@ provider "aws" {
   alias   = "region-worker"
 
 }
+
+terraform {
+  required_version = ">=0.15.0"
+  backend "s3" {
+    region  = "eu-west-2"
+    profile = "default"
+    key     = "terraformstatefile"
+    bucket  = "terraformstatebucket-1462021"
+  }
+}
